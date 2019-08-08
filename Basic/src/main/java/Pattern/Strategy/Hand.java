b608a3e815af6f8fc6e7e5e14a22f6de97c7be2c
PrivateHL <PrivateHL@163.com>
2019-08-08 18:37:23 +0800

    1

--- Basic/src/main/java/Pattern/Strategy/Hand.java
+++ Basic/src/main/java/Pattern/Strategy/Hand.java
@@ -1,6 +1,23 @@
 package Pattern.Strategy;
 
 public class Hand {
-    /**  **/
+    /** 表示石头的值 **/
     public static final int HANDVALUE_GUU = 0 ;
+    /** 表示剪刀的值 **/
+    public static final int HANDVALUE_CHO = 1 ;
+    /** 表示布的值 **/
+    public static final int HANDVALUE_PAA = 2 ;
+    /** 表示猜拳中的3种手势的实例 **/
+    public static final Hand[] hands = {
+            new Hand(HANDVALUE_GUU);
+            new Hand(HANDVALUE_CHO);
+            new Hand(HANDVALUE_PAA);
+    } ;
+    /** 表示猜拳中手势对应的字符串 **/
+    private static final String[] name = {  "石头","剪刀","布",  };
+    private int handvalue;
+    private Hand(int handvalue){
+        this.handvalue = handvalue;
+        }
+
 }
